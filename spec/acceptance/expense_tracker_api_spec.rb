5f3221f1beeccac3fb772f8df38d14fb6abcf124
sukanta-ghosh <sukanta.ghosh@xing.com>
2018-03-03 00:35:47 +0100

    Initial commit with acceptace test

---
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,47 @@
+require 'rack/test'
+require 'json'
+require_relative '../../app/api'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker Api' do
+    include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    it 'records the submitted expense' do
+      pending 'Need to persist expense'
+      coffee = post_expense(
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2018-03-01'
+        )
+
+      groceries = post_expense(
+        'payee' => 'Whole foods',
+        'amount' => 15.75,
+        'date'   => '2018-03-01'
+        )
+
+      zoo = post_expense(
+        'payee' => 'Zoo',
+        'amount' => 20.75,
+        'date'   => '2018-02-01'
+        )
+
+        get '/expenses/2018-03-01'
+        expect(last_response.status).to eq(200)
+        parsed = JSON.parse(last_response.body)
+        expect(parsed).to contain_exactly(coffee, groceries)
+     end
+  end
+end
